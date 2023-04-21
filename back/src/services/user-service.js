a7e7991ff6e57dcffbbe1adeb1a99f7d1802d166
minseong <alstjd055@gmail.com>
2023-04-21 23:16:16 +0900

    Feat: "추가 회원가입 service"
    회원가입 service를 저장하지 않고 이전에 커밋해서 저장 후 커밋했습니다.

--- back/src/services/user-service.js
+++ back/src/services/user-service.js
@@ -2,6 +2,8 @@
 // 비즈니스 로직이란 정보를 받는 (controller)와 데이터베이스와 소통하는(DB) 것 외에 모든 것.
 // 정보를 처리하는 곳
 const userModel = require('../db/models/user-model');
+/** 비밀번호 암호화하기위한 bcrypt package */
+const bcrypt = require('bcrypt');
 
 class UserService {
   /** 본 파일의 맨 아래에서, new UserService(userModel) 하면, 이 함수의 인자로 전달됌 */
@@ -9,15 +11,32 @@ class UserService {
     this.userModel = userModel;
   }
 
-// 회원가입
-async addUser(userInfo) {
-  /** 회원가입시 받는 사용자 정보 */
-  const { email, password, fullName, phoneNumber, address } = userInfo;
+  // 회원가입
+  async addUser(userInfo) {
+    /** 회원가입시 받는 사용자 정보 */
+    const { email, password, fullName, phoneNumber, address } = userInfo;
 
-  /** 이메일 중복 확인 */
+    /** 이메일 중복 확인 */
+    const user = await this.userModel.findByEmail(email);
+    if (user) {
+      throw new Error(
+        "이 이메일은 현재 사용중입니다. 다른 이메일을 입력해주세요."
+      );
+    }
 
+    /** 중복된 이메일이 없다면 회원가입 진행 */
 
-}
+    /** 우선 비밀번호 해쉬화(암호화) */
+    const hashedPassword = await bcrypt.hash(password, 10);
+    /** 해쉬화된 비밀번호로 사용자에게 받은 비밀번호를 대체하여 저장 */
+    const newUserInfo = { email, password: hashedPassword, fullName, phoneNumber, address };
+
+    /** 암호화된 비밀번호로 저장된 newUserInfo를 db에 저장 */
+    const createNewUser = await this.userModel.create(newUserInfo);
+
+    /** addUser 메서드를 실행하면 요청값 중 비밀번호만 해쉬화해서 db에 저장 */
+    return createNewUser;
+  }
 
 }
 
