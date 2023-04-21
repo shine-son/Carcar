d75c1c4ff6d413a92cd8e8ef257309e656ab3367
shine-son <schk9611@gmail.com>
2023-04-22 02:30:36 +0900

    Fix: async-handler 폴더 위치 변경

--- back/src/routers/order-router.js
+++ back/src/routers/order-router.js
@@ -1,67 +1,67 @@
 const { Router } = require("express");
 
-const { asyncHandler } = require("../middlewares/async-handler");
+const { asyncHandler } = require("../utils/async-handler");
 const { orderService } = require("../services/order-service");
 
 const orderRouter = Router();
 
 orderRouter.post(
-    "/",
-    asyncHandler(async (req, res, next) => {
-        const user_id = "6440131acd26489b4a21893e"; // user_id를 JWT 로직을 진행시켜서 찾아오기
-        const ordered = req.body; // req.body로 들어온 값 전달받기, 배열
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const user_id = "6440131acd26489b4a21893e"; // user_id를 JWT 로직을 진행시켜서 찾아오기
+    const ordered = req.body; // req.body로 들어온 값 전달받기, 배열
 
-        const order = await orderService.addOrder(ordered, user_id);
+    const order = await orderService.addOrder(ordered, user_id);
 
-        res.json(order);
-    })
+    res.json(order);
+  })
 );
 
 orderRouter.get(
-    "/",
-    asyncHandler(async (req, res, next) => {
-        const user_id = "6440131acd26489b4a21893e";
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const user_id = "6440131acd26489b4a21893e";
 
-        const order = await orderService.getOrderOfUser(user_id);
+    const order = await orderService.getOrderOfUser(user_id);
 
-        res.json(order);
-    })
+    res.json(order);
+  })
 );
 
 orderRouter.get(
-    "/:id",
-    asyncHandler(async (req, res, next) => {
-        // const user_id = "6440131acd26489b4a21893e"; // 로그인 여부만 확인하고 유저 정보는 필요없다? 아니면 더블체크를 해야한다...?
-        const order_id = req.params.id;
+  "/:id",
+  asyncHandler(async (req, res, next) => {
+    // const user_id = "6440131acd26489b4a21893e"; // 로그인 여부만 확인하고 유저 정보는 필요없다? 아니면 더블체크를 해야한다...?
+    const order_id = req.params.id;
 
-        const order = await orderService.getOrder(order_id);
+    const order = await orderService.getOrder(order_id);
 
-        res.json(order);
-    })
+    res.json(order);
+  })
 );
 
 //
 orderRouter.put(
-    "/:id",
-    asyncHandler(async (req, res, next) => {
-        const order_id = req.params.id;
-        const { address } = req.body;
+  "/:id",
+  asyncHandler(async (req, res, next) => {
+    const order_id = req.params.id;
+    const { address } = req.body;
 
-        const order = await orderService.updateOrder(order_id, address);
+    const order = await orderService.updateOrder(order_id, address);
 
-        res.json(order);
-    })
+    res.json(order);
+  })
 );
 
 orderRouter.delete(
-    "/:id",
-    asyncHandler(async (req, res, next) => {
-        const order_id = req.params.id;
+  "/:id",
+  asyncHandler(async (req, res, next) => {
+    const order_id = req.params.id;
 
-        const order = await orderService.deleteOrder(order_id);
+    const order = await orderService.deleteOrder(order_id);
 
-        res.json(order);
-    })
+    res.json(order);
+  })
 );
 
 module.exports = orderRouter;
