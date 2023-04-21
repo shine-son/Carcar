81a2d7061e64cfe3cda9a4b748884ccb9ebbc17b
shine-son <schk9611@gmail.com>
2023-04-22 02:40:46 +0900

    Fix: 코드 수정

--- back/src/db/models/order-model.js
+++ back/src/db/models/order-model.js
@@ -1,6 +1,69 @@
 const { Order, OrderedProduct } = require("../schemas/order-schema");
 
-class OrderModel {}
+class OrderModel {
+  // Order 인스턴스(데이터) 생성
+  async createOrder(user_id, ordered_product, address, total_price) {
+    return new Order({
+      user_id,
+      ordered_product,
+      address, // 나중에 변경 예정
+      total_price,
+    });
+  }
+
+  // OrderedProduct 인스턴스(데이터) 생성
+  async createOrderedProduct(product, amount) {
+    amount = Number(amount);
+    const price = Number(product.price);
+    const total_price = price * amount;
+    return new OrderedProduct({
+      product_id: product.product_id,
+      amount,
+      price,
+      image: product.image,
+      total_price,
+    });
+  }
+
+  // 주문 전체 조회
+  async getAllOrder() {
+    const orders = await Order.find({});
+
+    return orders;
+  }
+
+  // 주문 번호로 주문 조회
+  async getOrderById(order_id) {
+    const order = await Order.findOne({ order_id });
+
+    return order;
+  }
+
+  // 유저 id로 주문 조회
+  async getOrderByUserId(user_id) {
+    const order = await Order.find({ user_id });
+
+    return order;
+  }
+
+  // 유저 정보 업데이트
+  async updateOrderById(order_id, address) {
+    const order = await Order.findOneAndUpdate(
+      { order_id },
+      { address },
+      { new: true }
+    ); // new: true를 적용하면 업데이트된 객체를 반환한다.
+
+    return order;
+  }
+
+  // 유저 정보 삭제
+  async deleteOrderById(order_id) {
+    const order = await Order.deleteOne({ order_id });
+
+    return order;
+  }
+}
 
 const orderModel = new OrderModel();
 
