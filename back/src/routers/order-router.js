2fe484a2340ae370f6aafaef1cb2b7b829ad84f3
shine-son <schk9611@gmail.com>
2023-04-22 02:45:11 +0900

    Feat: order URD 기능 router 구현

---
+++ back/src/routers/order-router.js
@@ -0,0 +1,66 @@
+const { Router } = require("express");
+
+const { asyncHandler } = require("../utils/async-handler");
+const { orderService } = require("../services/order-service");
+
+const orderRouter = Router();
+
+orderRouter.post(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const user_id = req.currentUserId; // user_id를 JWT 로직을 진행시켜서 찾아오기
+    const ordered = req.body; // req.body로 들어온 값 전달받기, 배열
+
+    const order = await orderService.addOrder(ordered, user_id);
+
+    res.json(order);
+  })
+);
+
+orderRouter.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const user_id = req.currentUserId;
+
+    const order = await orderService.getOrderOfUser(user_id);
+
+    res.json(order);
+  })
+);
+
+orderRouter.get(
+  "/:id",
+  asyncHandler(async (req, res, next) => {
+    const order_id = req.params.id;
+
+    const order = await orderService.getOrder(order_id);
+
+    res.json(order);
+  })
+);
+
+//
+orderRouter.put(
+  "/:id",
+  asyncHandler(async (req, res, next) => {
+    const order_id = req.params.id;
+    const { address } = req.body;
+
+    const order = await orderService.updateOrder(order_id, address);
+
+    res.json(order);
+  })
+);
+
+orderRouter.delete(
+  "/:id",
+  asyncHandler(async (req, res, next) => {
+    const order_id = req.params.id;
+
+    const order = await orderService.deleteOrder(order_id);
+
+    res.json(order);
+  })
+);
+
+module.exports = orderRouter;
