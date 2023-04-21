3304d065b7678fc7f7ee5ec21e599be265620170
minseong <alstjd055@gmail.com>
2023-04-21 22:57:19 +0900

    Feat: "추가 회원가입 router"
    user-router.js 파일 내에 회원가입 router를 구성하였습니다"

--- back/src/routers/user-router.js
+++ back/src/routers/user-router.js
@@ -6,13 +6,41 @@
 // 라우팅 = HTTP 요청에 따라 알맞은 응답을 보내주는 경로를 설정해주는 일
 // 검증과는 다른 것이지 않나??
 
+// 일단 router에 기능을 다 짜보고 3계층으로 나누기로
+
 
 const { Router } = require('express');
 
 
 const userRouter = Router();
 
-// 회원가입(join)
+// 회원가입(join, /api/users)
+userRouter.post('/api/users/:id', async (req, res, next) => {
+  try {
+  /** 
+   * req.body에서 data 가져오기 
+   * 요청값 { email, password, fullName, phoneNumber, address } 구조할당
+  */
+  const { email, password, fullName, phoneNumber, address } = req.body;
+
+  /** 위 데이터를 db에 추가하기위해 service로 보내기 */
+  const newUser = await userService.addUser({
+    email,
+    password,
+    fullName,
+    phoneNumber,
+    address,
+  });
+
+  /** 
+   * 추가된 db 데이터를 다시 프론트로 보내줌
+   * 201 Created: 요청이 성공적이었으며 그 결과로 새로운 리소스가 생성되었습니다. 이 응답은 일반적으로 POST 요청 또는 일부 PUT 요청 이후에 따라옵니다.
+  */
+  res.status(201).json(newUser);
+  } catch(error) {
+    next(error);
+  }
+});
   // 이메일
     // 형식 검증
     // 중복여부 검증
