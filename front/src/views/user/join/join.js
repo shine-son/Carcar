bf1161fdef428d37a5c7a36e71afdb4b1e498af0
김채영 <cccodud@gmail.com>
2023-04-23 18:00:02 +0900

    Add: alert창 대신 input에 change 이벤트리스너를 이용한 유효성 검사 추가

--- front/src/views/user/join/join.js
+++ front/src/views/user/join/join.js
@@ -8,42 +8,137 @@ const detailAddressInput = document.querySelector('#detailAddressInput');
 const passwordInput = document.querySelector('#passwordInput');
 const passwordConfirmInput = document.querySelector('#passwordConfirmInput');
 const submitButton = document.querySelector('#submitButton');
-// const log = document.getElementById('log');
 
-addAllElements();
+//에러메시지 정리
+const errors = {
+    nameError: '이름은 3자 이상이어야 합니다.',
+    emailError: '올바른 이메일 주소를 입력해주세요.',
+    passwordError: '비밀번호는 11자 이상이어야 합니다.',
+    confirmPasswordError: '비밀번호가 일치하지 않습니다.',
+};
+
+// addAllElements();
 addAllEvents();
-
-// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-async function addAllElements() {}
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
     submitButton.addEventListener('click', handleSubmit);
 }
 
-fullNameInput.addEventListener('change', function () {
-    const value = fullNameInput.value;
+//이름 검사 함수
+function validateName() {
+    const name = fullNameInput.value.trim();
+    const error = document.querySelector('#fullNameInput + .error');
+    error.textContent = name && name.length >= 3 ? '' : errors.nameError;
+    return !!name && name.length >= 3;
+}
 
-    // 입력값이 3자 이하인 경우 error 클래스를 가진 p 태그를 생성하여 추가
-    if (value.length < 3) {
-        let error = fullNameInput.nextElementSibling;
+//이메일 검사 함수
+function validateEmail() {
+    const email = emailInput.value.trim();
+    const error = document.querySelector('#emailInput + .error');
+    const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    error.textContent = isValidEmail ? '' : errors.emailError;
+    return isValidEmail;
+}
 
-        if (!error || !error.classList.contains('error')) {
-            error = document.createElement('p');
-            error.classList.add('error');
-            fullNameInput.insertAdjacentElement('afterend', error);
-        }
-
-        error.textContent = '입력값이 너무 짧습니다.';
+//비밀번호 검사 함수
+function validatePassword() {
+    const password = passwordInput.value.trim();
+    const error = document.querySelector('#passwordInput + .error');
+    if (password.length < 11) {
+        error.textContent = errors.passwordError;
+        return false;
     } else {
-        // 입력값이 4자 이상인 경우 error 클래스를 가진 p 태그를 삭제
-        const error = fullNameInput.nextElementSibling;
-
-        if (error && error.classList.contains('error')) {
-            error.remove();
-        }
+        error.textContent = '';
+        return true;
     }
-});
+}
+
+//비밀번호 일치여부 검사 함수
+function validatePasswordConfirm() {
+    const password = passwordInput.value.trim();
+    const passwordConfirm = passwordConfirmInput.value.trim();
+    const error = document.querySelector('#passwordConfirmInput + .error');
+    if (password !== passwordConfirm) {
+        error.textContent = errors.confirmPasswordError;
+        return false;
+    } else {
+        error.textContent = '';
+        return true;
+    }
+}
+
+//빈값 검사
+function validateOthers() {
+    const phoneNumber = phoneNumberInput.value.trim();
+    const postalCode = postalCodeInput.value.trim();
+    const mainAdress = mainAddressInput.value.trim();
+    const detailAddress = detailAddressInput.value.trim();
+    if (!phoneNumber || !postalCode || !mainAdress || !detailAddress) {
+        return false;
+    }
+    return true;
+}
+
+// 모든 조건 검사 함수
+function validateAll() {
+    const isNameValid = validateName();
+    if (!isNameValid) {
+        return;
+    }
+    const isEmailValid = validateEmail();
+    if (!isEmailValid) {
+        return;
+    }
+    const isOthersValid = validateOthers();
+    if (!isOthersValid) {
+        return;
+    }
+    const isPasswordValid = validatePassword();
+    if (!isPasswordValid) {
+        return;
+    }
+    const isPasswordConfirmValid = validatePasswordConfirm();
+    if (!isPasswordConfirmValid) {
+        return;
+    }
+
+    submitButton.disabled = false;
+}
+
+// 이벤트 리스너 등록
+// fullNameInput.addEventListener('input', validateAll);
+// emailInput.addEventListener('change', validateAll);
+// passwordInput.addEventListener('change', validateAll);
+// passwordConfirmInput.addEventListener('change', validateAll);
+// phoneNumberInput.addEventListener('input', validateAll);
+// postalCodeInput.addEventListener('input', validateAll);
+// mainAddressInput.addEventListener('input', validateAll);
+// detailAddressInput.addEventListener('input', validateAll);
+function addAllEventListeners() {
+    const inputFields = [
+        fullNameInput,
+        emailInput,
+        passwordInput,
+        passwordConfirmInput,
+        phoneNumberInput,
+        postalCodeInput,
+        mainAddressInput,
+        detailAddressInput,
+    ];
+
+    inputFields.forEach((input) => {
+        input.addEventListener('input', validateAll);
+        if (input.type === 'email' || input.type === 'password') {
+            input.addEventListener('change', validateAll);
+        } else {
+            input.addEventListener('blur', validateAll);
+        }
+    });
+}
+
+addAllEventListeners();
 
 // 회원가입 진행
 async function handleSubmit(e) {
@@ -57,24 +152,6 @@ async function handleSubmit(e) {
     const addressDetail = detailAddressInput.value;
     const password = passwordInput.value;
     const passwordConfirm = passwordConfirmInput.value;
-
-    // 잘 입력했는지 확인
-    const isFullNameValid = fullName.length >= 2;
-    const isEmailValid = validateEmail(email);
-    const isPasswordValid = password.length >= 11;
-    const isPasswordSame = password === passwordConfirm;
-
-    if (!isFullNameValid || !isPasswordValid) {
-        return alert('이름은 2글자 이상, 비밀번호는 11글자 이상이어야 합니다.');
-    }
-
-    if (!isEmailValid) {
-        return alert('이메일 형식이 맞지 않습니다.');
-    }
-
-    if (!isPasswordSame) {
-        return alert('비밀번호가 일치하지 않습니다.');
-    }
 
     //객체 만들기
     const data = {
@@ -109,11 +186,3 @@ async function handleSubmit(e) {
     const result = await res.json();
     console.log(result);
 }
-
-const validateEmail = (email) => {
-    return String(email)
-        .toLowerCase()
-        .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        );
-};
