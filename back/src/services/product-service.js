c3ec5b3a35223f2c105fadbb6e343818a4d9131e
yubin <shin.yubin18@gmail.com>
2023-04-24 16:39:35 +0900

    ADD : product 파트 서비스 추가
    product-service, category-model 추가

---
+++ back/src/services/product-service.js
@@ -0,0 +1,24 @@
+const { categoryModel } = require("../db/models/category-model");
+const { productModel } = require("../db/models/product-model");
+
+class ProductService {
+  async getProductsAll() {
+    return productModel.getProductsAll()
+  }
+  async getProductById(product_id) {
+    return productModel.getProductById(product_id)
+  }
+  async addProduct(product) {
+    return productModel.addProduct(product)
+  }
+  async updateProduct(id, body, option) {
+    return productModel.updateProduct(id, body, option)
+  }
+  async deleteProduct(id) {
+    return productModel.updateProduct(id)
+  }
+}
+
+const productService = new ProductService();
+
+module.exports = { productService };
