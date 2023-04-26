495d1d6d80a2ac4e4c5e6821c81c1068a84577d0
채진 정 <jeong9175@gmail.com>
2023-04-26 23:56:23 +0900

    Add: main 페이지 퍼블리싱 및 API 연결 완료

--- front/src/views/product/main/main.js
+++ front/src/views/product/main/main.js
@@ -1,52 +1,54 @@
+import { addCommas } from '../../useful-function.js';
+
 const productList = document.getElementById("new_product_area");
 let productData = [];  // 상품데이터 배열로 받아오기
 
-fetch("http://localhost:8080/product", { credential: false })
-    .then((response) => {
-        return response.json();
+fetch("http://34.22.74.213:5000/api/product", { credential: false })
+    .then(res => {
+        return res.json();
     })
     .then((json) => {
-        console.log("response: ", json);
         productData = json;
         console.log(productData);
-        // html 동적 생성
+
         productData.forEach((product) => {
-            // 상품데이터 html과 연결하기
+            if (product === productData[6]) {
+                // 강제로 에러를 생성하여 조건에 맞으면 catch로 넘어가도록
+                throw new Error('forEach문 중단을 위한 강제 에러 생성');
+            }
+            
             const productElement = document.createElement("div");
-            productElement.className = 'new_product'
+            productElement.className = 'new_product';
             const productImage = document.createElement('img');
-            productImage.className = 'new_img'
+            productImage.className = 'new_img';
             const productTextArea = document.createElement('div');
-            productTextArea.className = 'new_product_info'  // 제품 정보 영역
+            productTextArea.className = 'new_product_info';
             const productName = document.createElement('p');
-            productName.className = 'list_name'     // 제품 이름
+            productName.className = 'list_name';
             const productDescription = document.createElement('p');
-            productDescription.className = 'list_sub'   // 제품 설명
+            productDescription.className = 'list_sub';
             const productPrice = document.createElement('p');
-            productPrice.className = 'list_price'   // 제품 가격
+            productPrice.className = 'list_price';
+
             productImage.src += product.image;
             productName.innerHTML += product.name;
             productDescription.innerHTML += product.description;
-            productPrice.innerHTML += product.price
+            productPrice.innerHTML += "KRW " + addCommas(product.price);
+
             productList.appendChild(productElement);
-            productElement.appendChild(productImage);
-            productElement.append(productName, productDescription, productPrice);
+            productElement.append(productImage, productTextArea);
+            productTextArea.append(productName, productDescription, productPrice);
         })
     })
-    .catch((error) => console.error("error: ", error));
+.catch((error) => console.error(error));
 
-
-$(document).ready( function() {
-    $('.carousel').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        autoplay: true,
-        autoplaySpeed: 3000
-    });
-})    
-
-
+$('.carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 3000
+});
    
