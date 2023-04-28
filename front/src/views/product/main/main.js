5447c731ad27d14c81391413425c21dc1a01c694
채진 정 <jeong9175@gmail.com>
2023-04-29 03:54:36 +0900

    Add: 상단바 적용

--- front/src/views/product/main/main.js
+++ front/src/views/product/main/main.js
@@ -75,7 +75,7 @@ fetch("http://34.22.74.213:5000/api/product", { credential: false })
               // 클릭 이벤트가 발생한 요소의 data-id 속성 값을 가져옵니다.
               const productId = this.getAttribute('data-id');
               // 해당 ID로 이동합니다.
-              location.href = `http://localhost:8080/product/${productId}`;
+              location.href = `http://localhost:8000/product/${productId}`;
           });
         })    
     })
@@ -90,4 +90,18 @@ $('.carousel').slick({
     autoplay: true,
     autoplaySpeed: 3000
 });
-   
+
+const goToMypage = document.querySelector('#goToMypage');
+const currentToken = localStorage.getItem('token');
+
+if (currentToken ===
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDQ5ZDNhOGMyZDFmNzgxYzVlZDIxZTciLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2ODI2OTc2MjR9.J2Z7Slgjqo_VWl66qn0aGLY-l0ejJ25nhuBtSCU90ZA'
+) {
+    goToMypage.addEventListener('click', () => {
+        window.location.href = '/user-management';
+    });
+} else {
+    goToMypage.addEventListener('click', () => {
+        window.location.href = '/mypage';
+    });
+}
