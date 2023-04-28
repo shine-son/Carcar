a96fe78fec5cbf29198b380a1de07833875a8757
coffeejoa <wjalsrb0923@gmail.com>
2023-04-29 00:21:45 +0900

    Fix: 구조 수정

--- front/src/app.js
+++ front/src/app.js
@@ -42,7 +42,7 @@ import path from 'path';
 
 const app = express();
 const viewsRouter = express.Router();
-
+const __dirname = path.resolve();
 // Content-Type: application/json 형태의 데이터를 인식하고 핸들링할 수 있게 함.
 app.use(express.json());
 
@@ -69,7 +69,7 @@ viewsRouter.use('/product_list', serveStatic('product_list', 'product/'));
 // 이 때 ${resource}.html 을 기본 파일로 설정함.
 //temp값이 undefined 일 때 기본적으로 빈 string 값이 들어간다
 function serveStatic(resource, temp = '', pathParam = null) {
-  const resourcePath = path.join(__dirname, `./views/${temp}/${resource}`);
+  const resourcePath = path.join(__dirname, `./src/views/${temp}/${resource}`);
   const option = { index: `${resource}.html` };
 
   if (pathParam) {
