9652f5595bbb34775f58276b1a1bb0b329c88497
minseong <alstjd055@gmail.com>
2023-04-28 21:37:52 +0900

    추가: "관리자페이지 카테고리 수정 오류코드"
    관리자 페이지에서 카테고리를 수정할 때 db에 없는 카테고리를 바꾸려고 하면 오류를 뱉어내게 작성하였습니다.
    ;

--- back/src/services/category-service.js
+++ back/src/services/category-service.js
@@ -10,6 +10,15 @@ class CategoryService {
     return newCategory;
   }
   async updateCategory(name, body, option) {
+    const selectedCategory = await categoryModel.findCategory(name);
+
+    // db에 없는 카테고리라면 에러처리
+    if (!selectedCategory) {
+      const err = new Error("찾으시는 카테고리를 다시 확인해주세요.");
+      err.status = 404;
+      throw err
+    }
+
     const updatedCategory = await categoryModel.updateCategory(
       name,
       body,
